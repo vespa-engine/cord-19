cb3343073d9b7db210bff7ba975e586e507cf82e
Valerij Fredriksen <valerijf@verizonmedia.com>
2020-03-26 12:12:56 +0100

    Create basic front page

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Router } from '@reach/router';
+import { Flex, Box } from 'rebass';
+import NavMenu from 'App/shared/components/NavMenu';
+import Main from './Main';
+import Error from 'App/shared/components/Error';
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <Flex flexDirection="column">
+      <Box width={1} bg="#3F9CD8">
+        <NavMenu />
+      </Box>
+      <Box width={1}>
+        <Router primary={false} component={React.Fragment}>
+          <Main path="/" />
+          <Error default />
+        </Router>
+      </Box>
+    </Flex>
+  );
 }
 
 export default App;
---
+++ src/App/shared/components/NavMenu.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Box } from 'rebass';
+import { Image, Menu } from 'semantic-ui-react';
+import Link from 'App/shared/components/Link';
+import logo from 'App/shared/img/VespaLogoWhite.png';
+
+const NavBar = styled(Menu)`
+  &&& {
+    height: 64px;
+    margin: 0;
+
+    border-bottom: 1px solid rgba(63, 157, 216, 0.25);
+
+    .item,
+    .item > a,
+    .item > a:not(.ui) {
+      color: white;
+      font-weight: bold;
+    }
+
+    .item > a:hover {
+      color: #ffc43c;
+    }
+
+    .ui.image {
+      width: 100px;
+    }
+  }
+`;
+
+function NavMenu() {
+  return (
+    <Box sx={{ paddingLeft: '16px', paddingRight: '16px' }} width={1}>
+      <NavBar secondary inverted fluid>
+        <Menu.Item fitted>
+          <Link to="/">
+            <Image src={logo} />
+          </Link>
+        </Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Link to="http://blog.vespa.ai/">Blog</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="https://twitter.com/vespaengine">Twitter</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="https://docs.vespa.ai/documentation/">Docs</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="https://github.com/vespa-engine">GitHub</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link to="https://github.com/vespa-engine/vespa/issues">
+              Issues
+            </Link>
+          </Menu.Item>
+        </Menu.Menu>
+      </NavBar>
+    </Box>
+  );
+}
+
+export default NavMenu;
