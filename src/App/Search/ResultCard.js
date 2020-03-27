926ffad54920357fe0ebf3590ba6197c460aabfd
Valerij Fredriksen <valerij92@gmail.com>
2020-03-27 09:37:35 +0100

    Add authors

--- src/App/Search/ResultCard.js
+++ src/App/Search/ResultCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Card } from 'semantic-ui-react';
 import Link from 'App/shared/components/Link';
@@ -31,25 +31,13 @@ const StyledCard = styled(Card)`
     border-top: 1px solid rgba(34, 36, 38, 0.1);
     padding: 0.5em;
   }
-
-  .cardLink {
-    width: 100%;
-    height: 100%;
-    color: inherit;
-    text-decoration: inherit;
-  }
-
-  .cardLink:hover {
-    cursor: pointer;
-    opacity: 0.7;
-  }
 `;
 
-function formatText(text) {
-  const fixedText = text
-    .replace(/<sep \/>/g, '…')
-    .replace(/<(\/?)hi>/g, '<$1b>');
-  if (!fixedText) return null;
+const highlightReplacer = (text, replace) =>
+  text.replace(/<(\/?)hi>/g, replace);
+const formatText = text => {
+  if (!text) return null;
+  const fixedText = highlightReplacer(text.replace(/<sep \/>/g, '…'), '<$1b>');
   return (
     <p
       dangerouslySetInnerHTML={{
@@ -57,7 +45,17 @@ function formatText(text) {
       }}
     />
   );
-}
+};
+
+const nameFormatter = ({ first, middle, last }) => {
+  if (!last) return first || middle;
+  const matches = [first, middle]
+    .filter(s => s)
+    .join(' ')
+    .match(/(?:(?=^|\s)(\w)|([A-Z]))/g);
+
+  return (matches ? matches.join('') + ' ' : '') + last;
+};
 
 function Date({ date }) {
   return !date ? null : (
@@ -67,20 +65,49 @@ function Date({ date }) {
   );
 }
 
+function AuthorsList({ authors }) {
+  const [showAll, setShowAll] = useState(false);
+  const onShowAll = e => {
+    e.preventDefault();
+    setShowAll(true);
+  };
+  if (!authors) return null;
+  const limit = showAll || authors.length < 12 ? authors.length : 10;
+  return (
+    <div>
+      {authors
+        .map(nameFormatter)
+        .slice(0, limit)
+        .join(', ')}
+      {limit < authors.length ? (
+        <>
+          {' '}
+          <a href="#root" onClick={onShowAll}>
+            and {authors.length - limit} more
+          </a>
+        </>
+      ) : null}
+    </div>
+  );
+}
+
 function ResultCard({
-  fields: { id, title, datestring, journal, abstract, body_text },
+  fields: { id, title, datestring, journal, abstract, body_text, authors },
 }) {
   const content = formatText(abstract || body_text);
   return (
     <StyledCard>
-      <Link to={`/article/${id}`} className="cardLink">
-        <Card.Header>{title}</Card.Header>
-        <Card.Meta>
-          {journal}
-          <Date date={datestring} />
-        </Card.Meta>
-        {content && <Card.Content>{content}</Card.Content>}
-      </Link>
+      <Card.Header>
+        <Link to={`${process.env.PUBLIC_URL}/article/${id}`}>
+          {highlightReplacer(title, '')}
+        </Link>
+      </Card.Header>
+      <Card.Meta>
+        {journal}
+        <Date date={datestring} />
+        <AuthorsList authors={authors} />
+      </Card.Meta>
+      {content && <Card.Content>{content}</Card.Content>}
     </StyledCard>
   );
 }
