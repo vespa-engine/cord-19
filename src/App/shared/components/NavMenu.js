583640bd5e11aae91dfee9e2405218e73f9937ab
Valerij Fredriksen <valerij92@gmail.com>
2020-03-27 15:56:11 +0100

    Follow prettier

--- src/App/shared/components/NavMenu.js
+++ src/App/shared/components/NavMenu.js
@@ -40,7 +40,9 @@ function NavMenu() {
         </Menu.Item>
         <Menu.Menu position="right">
           <Menu.Item>
-            <Link to="https://github.com/vespa-engine/cord-19/blob/master/README.md">Read more</Link>
+            <Link to="https://github.com/vespa-engine/cord-19/blob/master/README.md">
+              Read more
+            </Link>
           </Menu.Item>
         </Menu.Menu>
       </NavBar>
