4d90abcbbfeda9de2177cbf4ba92543f30e00bdd
Valerij Fredriksen <valerij92@gmail.com>
2020-03-27 15:54:06 +0100

    Add basic footer

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Router } from '@reach/router';
 import { Flex, Box } from 'rebass';
 import NavMenu from 'App/shared/components/NavMenu';
+import Footer from 'App/shared/components/Footer';
 import Main from './Main';
 import Search from './Search';
 import Error from 'App/shared/components/Error';
 
 function App() {
   return (
-    <Flex flexDirection="column">
+    <Flex flexDirection="column" minHeight="100vh">
       <Box width={1} bg="#3F9CD8">
         <NavMenu />
       </Box>
@@ -19,6 +20,7 @@ function App() {
           <Error default />
         </Router>
       </Box>
+      <Footer />
     </Flex>
   );
 }
