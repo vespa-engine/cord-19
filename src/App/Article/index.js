7f4891299b18bc6c2edfa01ceae2d308102d5d05
Jon Marius Venstad <venstad@gmail.com>
2020-03-30 16:55:21 +0200

    More draft implementation of Article view

--- src/App/Article/index.js
+++ src/App/Article/index.js
@@ -1,16 +1,62 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Loading, Error } from 'App/shared/components/Messages';
+import { Container, Header, Tab } from 'semantic-ui-react';
+import { Error, Loading } from 'App/shared/components/Messages';
 import { Get } from 'App/shared/Fetcher';
+import ResultCard from 'App/Search/ResultCard';
 
 function Content({ title, abstract }) {
   return (
-    <>
-      <h1>{title}</h1>
-      <h4>Abstract</h4>
-      {abstract}
-    </>
+    <Container text>
+      <Header as="h1">{title}</Header>
+      <Header as="h3">Abstract</Header>
+      <p>{abstract}</p>
+    </Container>
   );
+}
+
+function Related({ title }) {
+  const query = new URLSearchParams();
+  query.set('type', 'any');
+  query.set('hits', '3');
+  query.set('query', title);
+  query.set('summary', 'short');
+
+  const { loading, response, error } = Get(
+    '/search/?' + query.toString()
+  ).state();
+
+  if (loading) return <Loading message="Searching..." />;
+  if (error)
+    return <Error message={error.message || 'Unknown search error...'} />;
+
+  console.log(response);
+  if (!('children' in response.root)) return null;
+  return (
+    <Tab.Pane>
+      <React.Fragment>
+        {response.root.children.map((article, i) => (
+          <ResultCard key={i} {...article} />
+        ))}
+      </React.Fragment>
+    </Tab.Pane>
+  );
+}
+
+function CitedBy({ citedBy }) {
+  return citedBy.map((id, _) => {
+    const { loading, response, error } = Get(
+      `/document/v1/covid-19/doc/docid/${id}`
+    ).state();
+
+    if (loading) return <Loading message="Loading..." />;
+    if (error)
+      return (
+        <Error message={error.message || `Failed to load article #${id}`} />
+      );
+
+    console.log(response);
+    return <ResultCard key={id} {...response} />;
+  });
 }
 
 function Article({ id }) {
@@ -24,7 +70,23 @@ function Article({ id }) {
 
   console.log(response);
 
-  return <Content {...response.fields} />;
+  const panes = [
+    {
+      menuItem: 'Related',
+      render: () => <Related title={response.fields.title} />,
+    },
+    {
+      menuItem: 'Cited by',
+      render: () => <CitedBy citedBy={response.fields.cited_by} />,
+    },
+  ];
+
+  return (
+    <Container>
+      <Content {...response.fields} />
+      <Tab panes={panes} />
+    </Container>
+  );
 }
 
 export default Article;
