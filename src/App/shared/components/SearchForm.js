6f95dace31a56491d99e13d7e4ff8ffa224cd464
Valerij Fredriksen <valerij92@gmail.com>
2020-03-31 19:00:23 +0200

    Implement ranking

--- src/App/shared/components/SearchForm.js
+++ src/App/shared/components/SearchForm.js
@@ -11,12 +11,38 @@ const StyledSearchForm = styled(Form)`
     margin: 0 auto;
 
     .input {
-      width: 100%;
+      margin: 0;
+      flex: 1;
+    }
+
+    & .dropdown {
+      margin-left: 1em;
+      width: fit-content;
+      border: 1px solid rgba(34, 36, 38, 0.15);
+      border-radius: 0.28571429rem
+      box-shadow: none;
+      background: white;
+      padding: 0.78125rem 0.4rem;
     }
   }
 `;
 
-function SearchForm({ onSearch, query = '' }) {
+const rankings = [
+  {
+    text: 'Vespa BM25',
+    value: 'bm25',
+  },
+  {
+    text: 'Vespa nativeRank',
+    value: 'default',
+  },
+  {
+    text: 'Freshness',
+    value: 'freshness',
+  },
+];
+
+function SearchForm({ onSearch, ranking, showRanking, query = '' }) {
   const [currentQuery, setCurrentQuery] = useState(query);
 
   return (
@@ -28,6 +54,20 @@ function SearchForm({ onSearch, query = '' }) {
         onChange={(e, { value }) => setCurrentQuery(value)}
         value={currentQuery}
       />
+      {showRanking && (
+        <Form.Dropdown
+          icon="sort amount down"
+          defaultValue={
+            rankings.find(
+              ({ value }, i) => ranking === value || (!ranking && i === 0)
+            ).value
+          }
+          options={rankings.map((rnk, id) => ({ id, ...rnk }))}
+          onChange={(event, { value }) =>
+            onSearch({ query: currentQuery, ranking: value })
+          }
+        />
+      )}
     </StyledSearchForm>
   );
 }
