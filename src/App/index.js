ccb4b91f036be08d3407ff86bc5dd8a1fc60250f
Leandro Alves <leandroalves@verizonmedia.com>
2020-04-02 10:49:00 +0200

    change main view

--- src/App/index.js
+++ src/App/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Router } from '@reach/router';
 import { Flex, Box } from 'rebass';
 import NavMenu from 'App/shared/components/NavMenu';
-import Footer from 'App/shared/components/Footer';
 import Main from './Main';
 import Search from './Search';
 import Article from './Article';
@@ -11,18 +10,15 @@ import Error from 'App/shared/components/Error';
 function App() {
   return (
     <Flex flexDirection="column" minHeight="100vh">
-      <Box width={1} bg="#3F9CD8">
+      <Box width={1}>
         <NavMenu />
       </Box>
-      <Box width={1}>
-        <Router primary={false} component={React.Fragment}>
-          <Main path="/" />
-          <Search path="/search" />
-          <Article path="/article/:id" />
-          <Error default />
-        </Router>
-      </Box>
-      <Footer />
+      <Router primary={false} component={React.Fragment}>
+        <Main path="/" />
+        <Search path="/search" />
+        <Article path="/article/:id" />
+        <Error default />
+      </Router>
     </Flex>
   );
 }
