696b65a874ada64cd42a4c93c09c501600e0fbb8
Frode Lundgren <frodelu@yahoo-inc.com>
2020-04-16 22:36:21 +0200

    Add 'in the news' section with logos

--- src/App/Main.js
+++ src/App/Main.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Box, Text } from 'rebass';
-import { Grid, List, Image } from 'semantic-ui-react';
+import { Grid, List, Image, Divider } from 'semantic-ui-react';
 import { shuffle } from 'lodash';
 import SearchForm from 'App/shared/components/SearchForm';
 import Link from 'App/shared/components/Link';
 import { onSearch } from './Search/Utils';
 import VespaIcon from 'App/shared/img/VespaIcon.png';
 import Footer from 'App/shared/components/Footer';
+
+// Logos used in "featured in" section
+import CWLogo from 'App/shared/img/logos/computerworld-logo.png';
+import SMDLogo from 'App/shared/img/logos/search-marketing-daily.png';
+import ZDNetLogo from 'App/shared/img/logos/zdnet-logo-large.png';
+import DatanamiLogo from 'App/shared/img/logos/datanami-logo-2018.png';
+import WSJLogo from 'App/shared/img/logos/wsj-logo-big-black.svg';
+
+const featuredArticles = [
+  {
+    logo: CWLogo,
+    name: 'ComputerWorld',
+    link: 'https://www.computerworld.com/article/3534478/tech-pitches-in-to-fight-covid-19-pandemic.html'
+  },
+  {
+    logo: SMDLogo,
+    name: 'SearchMarketing Daily',
+    link: 'https://www.mediapost.com/publications/article/349673/verizon-media-launches-covid-19-search-engine-for.html'
+  },
+  {
+    logo: ZDNetLogo,
+    name: 'ZDNet',
+    link: 'https://www.zdnet.com/article/verizon-introduces-open-source-big-data-coronavirus-search-engine/'
+  },
+  {
+    logo: DatanamiLogo,
+    name: 'Datanami',
+    link: 'https://www.datanami.com/2020/04/10/yahoos-vespa-takes-a-whack-at-cord-19-data/'
+  },
+  {
+    logo: WSJLogo,
+    name: 'Wall Street Journal',
+    link: 'https://www.wsj.com/articles/machine-learning-experts-delve-into-47-000-papers-on-coronavirus-family-11586338201'
+  }
+];
 
 const sampleQueries = [
   '+covid-19 +temperature impact on viral transmission',
@@ -18,6 +53,10 @@ const sampleQueries = [
   '+("SARS-COV-2" "coronavirus 2" "novel coronavirus")',
   '+chloroquine +(covid-19 coronavirus)',
 ];
+
+const TallDivider = styled(Divider)`
+  padding-top: 100px;
+`;
 
 const Content = styled(Box)`
   background-image: linear-gradient(0deg, #98c1db 7%, #005a8e 100%);
@@ -101,6 +140,24 @@ function VespaDescription() {
   );
 }
 
+function FeaturedInSection() {
+  return (
+    <>
+      <TallDivider hidden />
+      <Text>In the news:</Text>
+      <List horizontal>
+        {featuredArticles.map((article, i) => (
+          <List.Item key={i}>
+            <Link to={article.link}>
+              <Image src={article.logo} alt={article.name} size="tiny" centered verticalAlign='middle' />
+            </Link>
+          </List.Item>
+        ))}
+      </List>
+    </>
+  )
+}
+
 function Main() {
   return (
     <Content width={1}>
@@ -110,6 +167,7 @@ function Main() {
           <SearchForm onSearch={onSearch} />
           <SearchSuggestions />
           <VespaDescription />
+          <FeaturedInSection />
         </Grid.Column>
       </ContentGrid>
       <Footer page="main" />
