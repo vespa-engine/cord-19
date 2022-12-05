383c58546d3854d84221dd13f6fc63a6400fe3c9
Jo Kristian Bergum <bergum@yahooinc.com>
2022-12-05 13:19:50 +0100

    Change logo and remove bloat

--- src/App/Article/index.js
+++ src/App/Article/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import Moment from 'react-moment';
 import { navigate } from '@reach/router';
-import { uniq } from 'lodash';
 import { Container, Header, Tab, List } from 'semantic-ui-react';
 import { Error, Loading } from 'App/shared/components/Messages';
 import { Get } from 'App/shared/Fetcher';
 import ResultCard from 'App/Search/ResultCard';
 import Link from 'App/shared/components/Link';
 import { nameFormatter } from 'App/shared/utils/formatter';
-import Pagination from 'App/shared/components/Pagination';
 import Footer from 'App/shared/components/Footer';
 
 const ContainerContent = styled(Container)`
@@ -106,6 +104,7 @@ function Related({ id, specter }) {
   query.set('id', id);
   query.set('summary', 'short');
   query.set('hits', 5);
+  query.set('collapsefield', 'title');
   if (specter) query.set('use-specter', true);
   const { loading, response, error } = Get(
     '/search/?' + query.toString()
@@ -115,7 +114,6 @@ function Related({ id, specter }) {
   if (error)
     return <Error message={error.message || 'Unknown search error...'} />;
 
-  console.log(response);
   if (!('children' in response.root)) return null;
   return (
     <Tab.Pane>
@@ -131,30 +129,6 @@ function Related({ id, specter }) {
   );
 }
 
-function CitedBy({ citedBy, total, offset, onOffsetChange }) {
-  return (
-    <Container>
-      {citedBy.slice(offset, offset + 10).map(id => (
-        <Citation key={id} id={id} />
-      ))}
-      <Pagination {...{ total, offset, onOffsetChange }} />
-    </Container>
-  );
-}
-
-function Citation({ id }) {
-  const { loading, response, error } = Get(
-    `/document/v1/covid-19/doc/docid/${id}?fieldSet=doc:title,abstract,doi,abstract_t5,journal,source,timestamp,license`
-  ).state();
-
-  if (loading) return <Loading message="Loading..." />;
-  if (error)
-    return <Error message={error.message || `Failed to load article #${id}`} />;
-
-  console.log(response);
-  return <ResultCard {...response} />;
-}
-
 function Article({ id }) {
   const url = new URL(window.location);
   const { loading, response, error } = Get(
@@ -165,41 +139,10 @@ function Article({ id }) {
   if (error)
     return <Error message={error.message || `Failed to load article #${id}`} />;
 
-  console.log(response);
-
-  const citations = uniq([
-    ...(response.fields.cited_by || []),
-    ...(response.fields.citations_inbound || [])
-      .map(c => c.source_id)
-      .filter(c => !isNaN(c)),
-  ]);
-
   const panes = [
     {
-      menuItem: 'Similar articles by Sent-SciBERT',
-      render: () => <Related id={response.fields.id} specter={false} />,
-    },
-    {
-      menuItem: 'Similar articles by SPECTER',
+      menuItem: 'Similar articles, powered by specter embedding search',
       render: () => <Related id={response.fields.id} specter={true} />,
-    },
-    {
-      menuItem: {
-        key: 'citations',
-        content: `${citations.length} citing articles`,
-        disabled: citations.length === 0,
-      },
-      render: () => (
-        <CitedBy
-          citedBy={citations}
-          offset={parseInt(url.searchParams.get('offset')) || 0}
-          total={citations.length}
-          onOffsetChange={offset => {
-            url.searchParams.set('offset', offset);
-            navigate(url);
-          }}
-        />
-      ),
     },
   ];
 
